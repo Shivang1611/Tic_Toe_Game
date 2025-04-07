b7eb0ecd56b95f20c2508977927b8f755247bf0c
Shivang1611 <shivangshukla306@gmail.com>
2025-04-08 00:29:54 +0530

    setting tic-toe-app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function() {
+  return (
+    <View>
+      <Text>tic toe</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
